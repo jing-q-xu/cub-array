ef051b392101f81fd0c989b26daa98a1115d405c
Darwin Yuan <darwin.yuan@hotmail.com>
2021-05-26 00:14:16 +0800

    added Placement

---
+++ test/ut/PlacementTest.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Darwin Yuan on 2021/5/26.
+//
+
+#include <catch.hpp>
+#include <cub/base/Placement.h>
+
+namespace {
+    constexpr short DE_MAGIC = 0xE7A2;
+    constexpr short CO_MAGIC = 0xABCD;
+    static bool cons = false;
+
+    struct Foo {
+        Foo(long long a) : a(a), b{CO_MAGIC} {
+            cons = true;
+        }
+
+        long long a;
+        short b;
+
+        ~Foo() {
+            b = DE_MAGIC;
+            cons = false;
+        }
+    };
+}
+
+SCENARIO("Placement") {
+    Placement<Foo> obj;
+
+    static_assert(alignof(obj) == alignof(Foo));
+
+    REQUIRE(!cons);
+
+    obj.Emplace(100);
+
+    REQUIRE(cons);
+    REQUIRE(obj->a == 100);
+    REQUIRE(obj->b == CO_MAGIC);
+
+    obj.Destroy();
+
+    REQUIRE(!cons);
+    REQUIRE(obj->b == DE_MAGIC);
+}
