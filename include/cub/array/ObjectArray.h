8f1c3d68503fdac334d79aa80c35e04e326558da
Darwin Yuan <darwin.yuan@hotmail.com>
2021-05-27 22:26:09 +0800

    refactored ObjectArray

--- include/cub/array/ObjectArray.h
+++ include/cub/array/ObjectArray.h
@@ -9,7 +9,19 @@
 #include <cub/array/detail/ArrayHolder.h>
 #include <cub/array/detail/ArrayLike.h>
 
+namespace detail {
+    template<typename T, std::size_t MAX_NUM, typename = void>
+    struct ObjectArrayTrait {
+        using Type = detail::ArrayLike<typename detail::ArrayHolderTrait<T>::template Type<MAX_NUM, Placement<T>>>;
+    };
+
+    template<typename T, std::size_t MAX_NUM>
+    struct ObjectArrayTrait<T, MAX_NUM, std::enable_if_t<std::is_trivially_constructible_v<T>>> {
+        using Type = detail::ArrayLike<typename detail::ArrayHolderTrait<T>::template Type<MAX_NUM>>;
+    };
+}
+
 template<typename T, std::size_t MAX_NUM>
-using ObjectArray = detail::ArrayLike<typename detail::ArrayHolderTrait<T>::template Type<MAX_NUM, Placement<T>>>;
+using ObjectArray = typename detail::ObjectArrayTrait<T, MAX_NUM>::Type;
 
 #endif //CUB_ARRAY_OBJECTARRAY_H
